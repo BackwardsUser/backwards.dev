47cc541e196ddb4a4870a7e70caa882e5fd81cdc
Backwards <AlexHardingYT@gmail.com>
2025-01-09 22:52:24 -0500

    Added an Icon Library and setup Landing page

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Hero from '$lib/components/Hero.svelte';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+	import ArrowDown from '~icons/material-symbols/keyboard-double-arrow-down-rounded';
+	import { crossfade, fade, fly } from 'svelte/transition';
+	import { onMount } from 'svelte';
+
+	let isAtTop = true;
+
+  onMount(() => {
+    const handleScroll = () => {
+      isAtTop = window.scrollY === 0;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
+</script>
+
+<header class="align-center flex h-screen flex-col justify-between p-4">
+  <div class=""></div>
+  <Hero></Hero>
+  <div 
+    class="transition-all duration-300 text-xl self-center ease-in-out relative"
+    style="transform: translateX({isAtTop ? '0px' : '-200px'})"
+    class:opacity-0={!isAtTop}
+  >
+    <div class="animate-bounce">
+      <ArrowDown></ArrowDown>
+    </div>
+  </div>
+</header>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,4 +1,4 @@
-<div class="" id="hero">
-  <h1 class="text-4xl">Backwards Development</h1>
-  <h4>Software, Scripts, Discord Bots, and Mods</h4>
+<div class="text-center h-max self-center" id="hero">
+  <h1 class="text-6xl">Backwards Development</h1>
+  <h4 class="text-2xl">Software, Scripts, Discord Bots, and Mods</h4>
 </div>
