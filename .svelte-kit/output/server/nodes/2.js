7fe1cf543b395268bffd8cb25532dcaf39a3eec0
BackwardsUser <alexander@backwards.dev>
2025-01-11 08:47:44 -0500

    Added .svelte-kit dir to gitignore

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.BhNerBaN.js","_app/immutable/chunks/disclose-version.BQGKy5kQ.js","_app/immutable/chunks/runtime.RJqc9H5L.js","_app/immutable/chunks/legacy.D29pJNd3.js","_app/immutable/chunks/svelte-head.UHu9QReN.js","_app/immutable/chunks/index-client.tGS7cQs6.js","_app/immutable/chunks/index.C9CopWQ3.js","_app/immutable/chunks/props.BYllJ5jM.js"];
-export const stylesheets = ["_app/immutable/assets/2.c6i8ireL.css"];
-export const fonts = [];
