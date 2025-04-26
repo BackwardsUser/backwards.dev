34a6ea0e8742acaf56790d7c627330a3f624210c
BackwardsUser <alexander@backwards.dev>
2025-04-26 11:45:49 -0400

    Fixed Brands missing from small client and added "schedule meeting" "brand"

--- src/lib/components/pages/Large.svelte
+++ src/lib/components/pages/Large.svelte
@@ -7,6 +7,7 @@
   import CibGitea from "~icons/cib/gitea";
   import MaterialSymbolsPerson from '~icons/material-symbols/person';
   import MaterialSymbolsMail from '~icons/material-symbols/mail';
+  import MaterialSymbolsCalendarClockOutline from '~icons/material-symbols/calendar-clock-outline';
 
 
   import Header from "$lib/components/cards/Header.svelte";
@@ -24,11 +25,12 @@
 
   const brands = [
     { url: "https://www.github.com/BackwardsUser", icon: MdiGithub },
-    { url: "https://api.backwards.dev/files/Alexander_Harding_Resume.docx", icon: MdiResume },
-    { url: "https://www.youtube.com/@BackwardsDevelopment", icon: MdiYoutube },
-    { url: "https://discord.gg/Zhq9yjhHKr", icon: MdiDiscord },
-    { url: "https://www.linkedin.com/in/alexander-harding-71b661265/", icon: MdiLinkedin },
     { url: "https://git.backwards.dev/", icon: CibGitea },
+    { url: "https://api.backwards.dev/files/Alexander_Harding_Resume.docx", icon: MdiResume },
+    { url: "https://www.linkedin.com/in/alexander-harding-71b661265/", icon: MdiLinkedin },
+    { url: "https://doodle.com/bp/backwards_user__/schedule-a-meeting", icon: MaterialSymbolsCalendarClockOutline },
+    { url: "https://discord.gg/Zhq9yjhHKr", icon: MdiDiscord },
+    { url: "https://www.youtube.com/@BackwardsDevelopment", icon: MdiYoutube },
   ];
 
   const downloads = [];
--- src/lib/components/pages/Small.svelte
+++ src/lib/components/pages/Small.svelte
@@ -3,7 +3,11 @@
   import MdiYoutube from "~icons/mdi/youtube";
   import MdiDiscord from "~icons/mdi/discord";
   import MdiLinkedin from "~icons/mdi/linkedin";
+  import MdiResume from '~icons/mdi/resume';
   import CibGitea from "~icons/cib/gitea";
+  import MaterialSymbolsPerson from '~icons/material-symbols/person';
+  import MaterialSymbolsMail from '~icons/material-symbols/mail';
+  import MaterialSymbolsCalendarClockOutline from '~icons/material-symbols/calendar-clock-outline';
 
   import Header from "../cards/Header.svelte";
   import Projects from "../containers/Projects.svelte";
@@ -14,14 +18,13 @@
   export let data;
 
   const brands = [
-    { url: "https://www.github.com/BackwardsUser", icon: MdiGithub },
-    { url: "https://www.youtube.com/@BackwardsDevelopment", icon: MdiYoutube },
-    { url: "https://discord.gg/Zhq9yjhHKr", icon: MdiDiscord },
-    {
-      url: "https://www.linkedin.com/in/alexander-harding-71b661265/",
-      icon: MdiLinkedin,
-    },
-    { url: "https://git.backwards.dev/", icon: CibGitea },
+    { url: "https://www.github.com/BackwardsUser", icon: MdiGithub, redirect: false },
+    { url: "https://git.backwards.dev/", icon: CibGitea, redirect: false },
+    { url: "https://api.backwards.dev/files/Alexander_Harding_Resume.docx", icon: MdiResume, redirect: false },
+    { url: "https://www.linkedin.com/in/alexander-harding-71b661265/", icon: MdiLinkedin, redirect: false },
+    { url: "https://doodle.com/bp/backwards_user__/schedule-a-meeting", icon: MaterialSymbolsCalendarClockOutline, redirect: true },
+    { url: "https://discord.gg/Zhq9yjhHKr", icon: MdiDiscord, redirect: false },
+    { url: "https://www.youtube.com/@BackwardsDevelopment", icon: MdiYoutube, redirect: false },
   ];
 </script>
 
