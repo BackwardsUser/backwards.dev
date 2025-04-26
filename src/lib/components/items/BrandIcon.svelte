2858901db5a4484fda02192cf410e8ba01f6ad44
BackwardsUser <alexander@backwards.dev>
2025-04-26 16:58:08 -0400

    Made icons redirect by default

--- src/lib/components/items/BrandIcon.svelte
+++ src/lib/components/items/BrandIcon.svelte
@@ -2,6 +2,6 @@
   export let url, icon, redirect;
 </script>
 
-<a href={url} target={ redirect = true ? "_blank" : "" } class="btn variant-filled-surface text-2xl p-3 rounded-md spin">
+<a href={url} target="_blank" class="btn variant-filled-surface text-2xl p-3 rounded-md spin">
   <span class="block spinner duration-500"><svelte:component this={icon}></svelte:component></span>
 </a>
